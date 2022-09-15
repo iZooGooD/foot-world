07caf5e3c40bee8596cddab79a1202a89fc9e31f
= <=>
2022-09-15 12:04:26 -0700

    Scaffolding the layout and adding required packages

---
+++ src/renderer/components/searchbox.jsx
@@ -0,0 +1,10 @@
+const Searchbox = () => {
+    return(
+        <div className="search-box">
+          <input className="custom-search-input" type="text" placeholder="Type your team's name to see the magic :)"/>
+          <button>SEARCH</button>
+      </div>
+    );
+}
+
+export default Searchbox;
---
+++ src/renderer/routes/home-layout.tsx
@@ -0,0 +1,14 @@
+import Searchbox from "renderer/components/searchbox";
+
+const HomeLayout = () =>{
+    return(
+      <div className="main-container">
+            <Searchbox />
+            <div className="main-content">
+              
+           </div>
+      </div>
+    );
+}
+
+export default HomeLayout;
