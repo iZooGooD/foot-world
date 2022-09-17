904a1e3a83fe1e79bbdbe9a138d60bc546e7e91d
= <=>
2022-09-17 13:01:20 -0700

    add loader and few styling

--- src/renderer/components/matchcard.jsx
+++ src/renderer/components/matchcard.jsx
@@ -6,14 +6,15 @@ import {
   buildLinksForDownload,
   download,
 } from 'renderer/utils/operations';
+import { ColorRing } from 'react-loader-spinner';
 
 const Matchcard = (props) => {
   const { title, backgroundImageLink, matchLink, source } = props.matchDetails;
-  const [downloadLinks,setDownloadLinks] = useState([]);
+  const [downloadLinks, setDownloadLinks] = useState([]);
   const [hasGeneratedDownloadLink, setHasGeneratedDownloadLink] =
     useState(false);
-  const [hasStartedDownload, setHasStartedDownload] =
-    useState(false);
+  const [hasStartedDownload, setHasStartedDownload] = useState(false);
+  const [hasFinishedDownloading, setHasFinishedDownloading] = useState(false);
   const [downloadProgress, setDownloadProgress] = useState({});
   const onDownload = async () => {
     const link = await findMp3u8Link(matchLink);
@@ -24,8 +25,12 @@ const Matchcard = (props) => {
 
   const onStartDownload = async (url, format, fileName) => {
     setHasStartedDownload(true);
-    download(url, format, fileName, (status) => {
+    await download(url, format, fileName, (status) => {
       setDownloadProgress(status);
+      if (status.segmentProcessed === status.maxSegments) {
+        setHasStartedDownload(false);
+        setHasFinishedDownloading(true);
+      }
     });
   };
 
@@ -35,27 +40,56 @@ const Matchcard = (props) => {
         <img className="source_image" src={backgroundImageLink} alt={title} />
       </div>
       <div className="video-information">
-        <p>{title}</p>
-        <p>Source: {source}</p>
-        <button type="button" onClick={onDownload} className="download-button">
-          Download
-        </button>
+        <p className="match-title">Title: {title}</p>
+        <small className="match-source">Source: {source}</small>
+        <ColorRing
+          height="50"
+          width="50"
+          colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+          ariaLabel="circles-loading"
+          wrapperStyle={{}}
+          wrapperClass="spinner-loader"
+          visible={hasStartedDownload}
+        />
         {hasGeneratedDownloadLink === true && (
           <div className="generated_links">
+            <p>Available formats:</p>
             {downloadLinks.map((item) => (
-              <button type="button" onClick={()=>onStartDownload(item.downloadLink,item.resolution,title)}>{item.resolution}</button>
+              <button
+                disabled={hasStartedDownload}
+                className="formats-button"
+                type="button"
+                onClick={() =>
+                  onStartDownload(item.downloadLink, item.resolution, title)
+                }
+              >
+                {item.resolution}
+              </button>
             ))}
           </div>
         )}
         {hasStartedDownload === true && downloadProgress.progress !== 100.0 && (
           <div className="download-stats">
             <p>
-              Download progress:{downloadProgress.progress}%{' '}
+              Download progress: {downloadProgress.progress}%{' '}
               {downloadProgress.mbDownloaded}mb
             </p>
           </div>
         )}
+        {hasFinishedDownloading && (
+          <small>
+            Your file was successfully downloaded to your downloads folder.
+          </small>
+        )}
       </div>
+      <button
+        type="button"
+        disabled={hasStartedDownload}
+        onClick={onDownload}
+        className="download-button"
+      >
+        Download
+      </button>
     </div>
   );
 };
